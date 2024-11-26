3406c8ef8c385948b2b31193cc592324c9bc289c
oblivious <thanos.papap42@gmail.com>
2024-11-26 15:00:38 +0200

    Updated license with special provision.

--- src/EmeraldsTable.h
+++ src/EmeraldsTable.h
@@ -1,19 +1,28 @@
 /**
  * DOP table, open addressing, linear probing, komihash hashing.
- * Copyright (C) 2024  oblivious
-
+ *
+ * Copyright (C) 2021-2025 oblivious
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As a special exception, this library may be used in programs licensed
+ * under any terms.  Modifications to the library itself must be licensed
+ * under the GNU General Public License version 3, with the inclusion of
+ * this special exception, while modifications to programs using this
+ * library may continue to be licensed under any terms.  This exception
+ * does not impose any additional licensing requirements, modify or
+ * transform the licensing terms of programs using this library.
  */
 
 #ifndef __EMERALDS_HASHTABLE_H_
